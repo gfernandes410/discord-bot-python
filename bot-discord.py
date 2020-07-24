3dd9916dc925ea635e33ffa4eeba0a38e996922e
Gabriel Fernandes <gfernandes410@gmail.com>
2020-07-24 01:09:48 -0300

    update

--- bot-discord.py
+++ bot-discord.py
@@ -12,26 +12,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix="!")
 client = discord.Client()
-
-async def load_sound_source(file):
-    return await discord.FFmpegOpusAudio.from_probe(file)
-
-async def load_voice_channel(ctx):
-    author = ctx.message.author
-    channel = author.voice.channel.id
-    channel = bot.get_channel(channel)
-
-    if ctx.voice_client:
-        await disconnect_channel(ctx)
-
-    return await channel.connect()        
-
-async def disconnect_channel(ctx):
-    if ctx.voice_client:
-        await ctx.voice_client.disconnect()
-
-async def on_ready():
-    print ("Ready")
+zz
 
 @bot.command(pass_context=True)
 async def leave(ctx):
@@ -73,6 +54,13 @@ async def missao(ctx):
 async def missaobugada(ctx):
     voiceChannel = await load_voice_channel(ctx)
     source = await load_sound_source('./sounds/missaobugada.mp3')
+    voiceChannel.play(source)
+    voiceChannel.is_playing()
+
+@bot.command(pass_context=True)
+async def gremio(ctx):
+    voiceChannel = await load_voice_channel(ctx)
+    source = await load_sound_source('./sounds/gremio.mp3')
     voiceChannel.play(source)
     voiceChannel.is_playing()
 
